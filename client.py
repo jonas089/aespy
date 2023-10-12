3eed1466fb0770bc579d7dd8fc386567c2693222
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2023-10-12 12:59:58 +0200

    message struct

--- client.py
+++ client.py
@@ -1,5 +1,22 @@
 import requests, json
 
+class Message:
+    def __init__(self, id, message, recipient, sender, iv):
+        self.id = id
+        self.message = message
+        self.recipient = recipient
+        self.sender = sender
+        self.iv = iv
+    
+    def as_json(self):
+        return json.dumps({
+            "id": self.id,
+            "message": self.message,
+            "recipient": self.recipient,
+            "sender": self.sender,
+            "iv": self.iv
+        })
+
 class MsgClient:
     def __init__(self, host, port):
         self.host = host
@@ -12,14 +29,8 @@ class MsgClient:
             "publickey":publickey
         }), headers=self.headers).json()
     
-    def put_message(self, id, message, recipient, sender, iv):
-        return requests.post((self.url+'/put'), data=json.dumps({
-            "id": id,
-            "message": message,
-            "recipient": recipient,
-            "sender": sender,
-            "iv": iv
-        }), headers=self.headers)
+    def put_message(self, message):
+        return requests.post((self.url+'/put'), data=message.as_json(), headers=self.headers)
 
     def get_user(self, id):
         return requests.get((self.url+'/get/user/{}').format(id)).json()
